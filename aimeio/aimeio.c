53e9d58ef2adca3984b525b9073d053dc3ac9739
Fang Lu <cc2lufang@gmail.com>
2020-02-07 20:24:47 -0600

    aimeio: Support Felica cards on libnfc readers through nfc-list

--- aimeio/aimeio.c
+++ aimeio/aimeio.c
@@ -16,6 +16,7 @@
 struct aime_io_config {
     wchar_t aime_path[MAX_PATH];
     wchar_t felica_path[MAX_PATH];
+    wchar_t nfclist_path[MAX_PATH];
     bool felica_gen;
     uint8_t vk_scan;
 };
@@ -61,6 +62,14 @@ static void aime_io_config_read(
             L"DEVICE\\felica.txt",
             cfg->felica_path,
             _countof(cfg->felica_path),
+            filename);
+
+    GetPrivateProfileStringW(
+            L"aime",
+            L"nfclistPath",
+            L"",
+            cfg->nfclist_path,
+            _countof(cfg->nfclist_path),
             filename);
 
     cfg->felica_gen = GetPrivateProfileIntW(
@@ -173,6 +182,97 @@ void aime_io_fini(void)
 {
 }
 
+static int aime_io_read_nfclist(
+        const wchar_t *path,
+        uint8_t *bytes,
+        size_t nbytes)
+{
+    SECURITY_ATTRIBUTES sattr;
+    HANDLE cpout_w = NULL, cpout_r = NULL;
+    PROCESS_INFORMATION procinfo;
+    STARTUPINFOW startinfo;
+    BOOL success = FALSE;
+    DWORD n;
+    char buf[65536], *ptr;
+    wchar_t cmdline[32];
+    int r, i, v;
+
+    sattr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sattr.bInheritHandle = TRUE;
+    sattr.lpSecurityDescriptor = NULL;
+
+    if (!CreatePipe(&cpout_r, &cpout_w, &sattr, 0)) {
+        dprintf("AimeIO DLL: CreatePipe failed\n");
+        return -1;
+    }
+    if (!SetHandleInformation(cpout_r, HANDLE_FLAG_INHERIT, 0)) {
+        dprintf("AimeIO DLL: SetHandleInformation failed\n");
+        return -1;
+    }
+
+    ZeroMemory(&procinfo, sizeof(PROCESS_INFORMATION));
+    ZeroMemory(&startinfo, sizeof(STARTUPINFOW));
+    startinfo.cb = sizeof(STARTUPINFO);
+    startinfo.hStdError = cpout_w;
+    startinfo.hStdOutput = cpout_w;
+    startinfo.hStdInput = cpout_r;
+    startinfo.dwFlags |= STARTF_USESTDHANDLES;
+    wcscpy(cmdline, L"nfc-list -t 2");
+
+    success = CreateProcessW(
+        path,          // binary path
+        cmdline,       // command line
+        NULL,          // process security attributes
+        NULL,          // primary thread security attributes
+        TRUE,          // handles are inherited
+        0,             // creation flags
+        NULL,          // use parent's environment
+        NULL,          // use parent's current directory
+        &startinfo,    // STARTUPINFO pointer
+        &procinfo);    // receives PROCESS_INFORMATION
+
+    // If an error occurs, exit the application.
+    if (!success) {
+        dprintf("AimeIO DLL: CreateProcess %S failed\n", path);
+        return -1;
+    }
+
+    if (!ReadFile(cpout_r, buf, 65536, &n, NULL)) {
+        dprintf("AimeIO DLL: Read failed\n");
+        return -1;
+    }
+    buf[n] = '\0';
+    ptr = strstr(buf, "ID (NFCID2): ");
+    if (!ptr) {
+        return 0;
+    }
+
+    // Felica found
+    ptr += 13;
+
+    memset(bytes, 0, nbytes);
+
+    for (i = 0 ; i < nbytes ; i++) {
+        r = sscanf(ptr, "%02X ", &v);
+        if (r != 1) {
+            dprintf("AimeIO DLL: %S: sscanf[%i] failed: %i\n",
+                    path,
+                    (int) i,
+                    r);
+            return -1;
+        }
+        bytes[i] = v;
+        ptr += 4;
+    }
+
+    WaitForSingleObject(procinfo.hProcess, 3000);
+    CloseHandle(procinfo.hProcess);
+    CloseHandle(procinfo.hThread);
+    CloseHandle(cpout_w);
+    CloseHandle(cpout_r);
+    return 1;
+}
+
 HRESULT aime_io_nfc_poll(uint8_t unit_no)
 {
     bool sense;
@@ -186,6 +286,20 @@ HRESULT aime_io_nfc_poll(uint8_t unit_no)
 
     aime_io_aime_id_present = false;
     aime_io_felica_id_present = false;
+
+    /* Read nfc-list */
+
+    if (aime_io_cfg.nfclist_path[0]) {
+        int ret = aime_io_read_nfclist(
+                aime_io_cfg.nfclist_path,
+                aime_io_felica_id,
+                sizeof(aime_io_felica_id));
+
+        if (ret > 0) {
+            aime_io_felica_id_present = true;
+            return S_OK;
+        }
+    }
 
     /* Don't do anything more if the scan key is not held */
 
